f3838c09fdf7643795dbebfb162c53dbfa8f96ef
svalgyx <154222657+svalgyx@users.noreply.github.com>
2024-02-23 18:23:14 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace HW3_3
     {
         static void Main(string[] args)
         {
-            ArrayBase[] arrays = new ArrayBase[10];
-            Random rnd = new Random();
+            Array[] arrays = new Array[10];
+            Rnd rnd = new Rnd();
             for (int i = 0; i < arrays.Length; i++)
             {
                 int num = rnd.Next(1, 4);
                 if (num == 1)
                 {
-                    SingleArray single_array = new SingleArray("no", rnd.Next(1, 11));
+                    Single single_array = new Single("no", rnd.Next(1, 11));
                     single_array.Create();
                     arrays[i] = single_array;
                 }
@@ -22,14 +22,14 @@ namespace HW3_3
                 {
                     int rows = rnd.Next(1, 11);
                     int columns = rnd.Next(1, 11);
-                    MatrixArray matrix_array = new MatrixArray("no", rows, columns);
+                    Matrix matrix_array = new Matrix("no", rows, columns);
                     matrix_array.Create();
                     arrays[i] = matrix_array;
                 }
                 if (num == 3)
                 {
                     int jagged_rows = rnd.Next(1, 11);
-                    JaggedArray jagged_array = new JaggedArray("no", jagged_rows);
+                    Jagged jagged_array = new Jagged("no", jagged_rows);
                     jagged_array.Create();
                     arrays[i] = jagged_array;
                 }
